c350422c84679c0bb701c0dfabb91ed8043f1ec1
OlsonLiu <32691728+OlsonLiu@users.noreply.github.com>
2017-10-30 01:14:35 +0800

    Create HelloWorld.js

---
+++ HelloWorld.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+var page = require('webpage').create();
+page.open("http://slimerjs.org", function(status){
+     if (status == "success") {
+         console.log("The title of the page is: "+ page.title);
+     }
+     else {
+         console.log("Sorry, the page is not loaded");
+     }
+     	page.render('test.png');
+		page.close();
+		slimer.exit();
+	});
